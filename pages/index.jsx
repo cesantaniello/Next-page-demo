6cabb62e2b826f4d64a2f7e922e5f948fff81f8d
Carlos Santaniello <carlos.e.santaniello@gmail.com>
2022-04-11 20:55:52 +0200

    Agrega Layouts

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,32 +1,17 @@
-import Head from 'next/head'
 import Link from 'next/link'
-import { Navbar } from '../components/Navbar'
-import styles from '../styles/Home.module.css'
+import { MainLayout } from '../components/layouts/MainLayout'
 
 export default function HomePage() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Home - Carlos</title>
-        <meta name="description" content="Home Page" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <MainLayout>
+      <h1 className={'title'}>
+        Ir a <Link href="/about"><a>About</a></Link>
+      </h1>
 
-      <Navbar />
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          {/* Ir a <a href="/about">About</a> */}
-          Ir a <Link href="/about"><a>About</a></Link>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-      </main>
-      
-    </div>
+      <p className={'description'}>
+        Get started by editing{' '}
+        <code className={'code'}>pages/index.js</code>
+      </p>
+    </MainLayout>
   )
 }
