9c1632c57cbb3ca0b67c085a0b95bb09c3dc4404
Carlos Santaniello <carlos.e.santaniello@gmail.com>
2022-04-11 16:56:31 +0200

    Next/Link

--- pages/about.js
+++ pages/about.js
@@ -2,28 +2,28 @@ import Head from 'next/head'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
-export default function HomePage() {
+export default function AboutPage() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Home - Carlos</title>
-        <meta name="description" content="Home Page" />
+        <title>About - Carlos</title>
+        <meta name="description" content="About Page" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          {/* Ir a <a href="/about">About</a> */}
-          Ir a <Link href="/about"><a>About</a></Link>
+          {/* Ir a <a href="/">Home</a> */}
+          Ir a <Link href="/"><a>Home</a></Link>
         </h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          <code className={styles.code}>pages/about.js</code>
         </p>
 
       </main>
-      
+
     </div>
   )
 }
